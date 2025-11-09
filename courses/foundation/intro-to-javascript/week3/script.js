9742dff5b5cc2630d6f6f78f8dd6a853edf5fa06
MKanunnikau <matea.ekv@gmail.com>
2025-11-09 19:23:51 +0100

    add the future age calculator

---
+++ courses/foundation/intro-to-javascript/week3/script.js
@@ -0,0 +1,6 @@
+// Age-ify (A future age calculator)
+const yearOfBirth = 1989;
+const yearOfFuture = 2048;
+const age = yearOfFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearOfFuture);
