d7ce514c888eed5d3189fe7635d3e3efd4729735
MKanunnikau <matea.ekv@gmail.com>
2025-11-09 19:47:54 +0100

    add the house price estimator

--- courses/foundation/intro-to-javascript/week3/script.js
+++ courses/foundation/intro-to-javascript/week3/script.js
@@ -22,3 +22,19 @@ if (shouldShowResultInDogYears === true) {
     `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
   );
 }
+
+//Housey pricey (A house price estimator)
+
+const estimatedHousePricePeter = 8 * 10 * 10 * 2.5 * 1000 + 100 * 300;
+const actualHousePricePeter = 2500000;
+const estimatedHousePriceJulia = 5 * 11 * 8 * 2.5 * 1000 + 70 * 300;
+const actualHousePriceJulia = 1000000;
+
+console.log(
+  "Is it true that the house Peter wants is overpriced? " +
+    (actualHousePricePeter > estimatedHousePricePeter)
+);
+console.log(
+  "Is it true that the house Julia wants is overpriced? " +
+    (actualHousePriceJulia > estimatedHousePriceJulia)
+);
