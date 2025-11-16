5532ec434f601f99e705fb71c45fe627352060d5
MKanunnikau <matea.ekv@gmail.com>
2025-11-16 22:41:26 +0100

    add the function which determines the day of the event given a number

--- courses/foundation/intro-to-javascript/week4/script.js
+++ courses/foundation/intro-to-javascript/week4/script.js
@@ -15,3 +15,39 @@ console.log(fullName1, fullName2); // Benjamin Hughes Jane Doe
 /*But what if the person is a woman? Describe how you would fix the getFullName so it also works for women
 
 - instead of passing a boolean value, we could pass a string with a requested formal name. */
+//---------------------------------------------------------
+
+//Event application for a calendar
+
+function getEventWeekday(daysFromNow) {
+  const date = new Date();
+  let eventDayNumber = (date.getDay() + daysFromNow) % 7; //make sure to wrap the count to 0 after 7 days
+
+  let eventDay = "";
+
+  switch (eventDayNumber) {
+    case 0:
+      eventDay = "Sunday";
+      break;
+    case 1:
+      eventDay = "Monday";
+      break;
+    case 2:
+      eventDay = "Tuesday";
+      break;
+    case 3:
+      eventDay = "Wednesday";
+      break;
+    case 4:
+      eventDay = "Thursday";
+      break;
+    case 5:
+      eventDay = "Friday";
+      break;
+    default:
+      eventDay = "Saturday";
+  }
+  return eventDay;
+}
+
+console.log(getEventWeekday(13));
