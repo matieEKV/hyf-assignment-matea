3a5c676600223ee5deb960b393dc1408db1972ef
MKanunnikau <matea.ekv@gmail.com>
2025-11-24 21:41:50 +0100

    add a function that calculates time needed to reach a destination

--- courses/foundation/intro-to-javascript/week5/script.js
+++ courses/foundation/intro-to-javascript/week5/script.js
@@ -22,3 +22,28 @@ for (let name of names) {
 // Code done
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'Katrine', 'Tala']
+//Exercise 1 - given speed and distance, write a function which returns the time it will take to arrive at your destination.
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function calculateTimeOfTravel(travelInfo) {
+  const speed = travelInfo.speed;
+  const distance = travelInfo.destinationDistance;
+  const hours = Math.floor(distance / speed);
+  let minutes = 0;
+  const remainder = distance % speed;
+
+  if (remainder > 0) {
+    minutes = Math.floor((remainder * 60) / speed);
+  }
+
+  return minutes != 1
+    ? `${hours} hours and ${minutes} minutes`
+    : `${hours} hours and ${minutes} minute`;
+}
+
+const travelTime = calculateTimeOfTravel(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
