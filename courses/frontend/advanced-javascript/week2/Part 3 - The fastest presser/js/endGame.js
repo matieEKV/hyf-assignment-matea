ff6327e93b5990072fcb5240238630b397cd3d62
MKanunnikau <matea.ekv@gmail.com>
2026-03-25 08:40:13 +0100

    update winner style

--- courses/frontend/advanced-javascript/week2/Part 3 - The fastest presser/js/endGame.js
+++ courses/frontend/advanced-javascript/week2/Part 3 - The fastest presser/js/endGame.js
@@ -11,11 +11,15 @@ function getWinner() {
   gameState.playerData.forEach((player) => {
     if (player.score === scoresArray[0] && player.score > 0) {
       if (player.playerBox) {
-        player.playerBox.style.background = "rgb(121, 239, 121)";
+        player.keyDisplay.textContent = `${player.key.toUpperCase()} WON!`;
+        player.keyDisplay.style.background = "rgb(121, 239, 121)";
+        player.playerBox.style.border = "15px solid rgb(121, 239, 121)";
         console.log("players score", player.score, scoresArray[0]);
       }
     } else {
-      player.playerBox.style.background = "rgb(239, 150, 150)";
+      player.keyDisplay.textContent = `${player.key.toUpperCase()} LOST!`;
+      player.keyDisplay.style.background = "rgb(239, 150, 150)";
+      player.playerBox.style.border = "15px solid rgb(239, 150, 150)";
     }
   });
 }
