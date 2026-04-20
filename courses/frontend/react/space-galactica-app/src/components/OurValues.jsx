97e1e2e92d53da716533cceed7acb3efb0139f68
MKanunnikau <matea.ekv@gmail.com>
2026-04-20 21:11:40 +0200

    add a OurValues component

---
+++ courses/frontend/react/space-galactica-app/src/components/OurValues.jsx
@@ -0,0 +1,32 @@
+export const OurValues () => {
+    return (
+    <>
+      <p className="values">
+        <span className="numbers">01</span>Exploration: We are driven by a
+        deep-seated desire to explore the unknown. We believe that the pursuit
+        of discovery is at the heart of human nature, and we are committed to
+        pushing the boundaries of what is possible
+      </p>
+      <p className="values">
+        <span className="numbers">02</span>Innovation: At Galactica, we
+        prioritize cutting-edge technology and innovation. We are constantly
+        evolving our spacecraft, safety protocols, and services to ensure that
+        our travelers experience the most advanced and secure space journeys
+        available.
+      </p>
+      <p className="values">
+        <span className="numbers">03</span>Sustainability: We are committed to
+        making space exploration sustainable for future generations. Our space
+        missions are designed to minimize environmental impact, both on Earth
+        and in space, and to foster a spirit of responsibility towards our
+        universe.
+      </p>
+      <p className="values">
+        <span className="numbers">04</span>Community: We believe in the power of
+        collective exploration. Our journeys are not just about reaching new
+        destinations; they are about building a community of space enthusiasts
+        who share a passion for the stars.
+      </p>
+    </>
+  );
+};
