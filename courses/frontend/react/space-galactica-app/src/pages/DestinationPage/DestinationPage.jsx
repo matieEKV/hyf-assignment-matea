0fbba103d609f4cb40721528ce81958d9a7772f7
MKanunnikau <matea.ekv@gmail.com>
2026-05-10 20:42:51 +0200

    refactor to lighten up a component as per reviewer's comment for week2 assignment

--- courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
@@ -3,6 +3,7 @@ import styles from "./DestinationPage.module.css";
 import { PlanetCard } from "../../components/PlanetCard.jsx";
 import { AddWishlistItem } from "./AddWishlistItem";
 import { PlanetsWishlistItem } from "./PlanetsWishlistItem";
+import Planets from "./planets.jsx";
 
 export const Destinations = () => {
   const [planetsWishlist, setPlanetsWishlist] = useState([]);
@@ -68,42 +69,20 @@ export const Destinations = () => {
         </section>
         <section className="card">
           <h2>Possible destinations</h2>
-          <PlanetCard
-            name="EUROPA"
-            description="Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers."
-            thumbnail="/destination/image-europa.png"
-            isSelected={isPlanetInWishlist("Europa")}
-            togglePlanetSelection={() =>
-              togglePlanetSelection("Europa", "/destination/image-europa.png")
-            }
-          />
-          <PlanetCard
-            name="MARS"
-            description="Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth."
-            thumbnail="/destination/image-mars.png"
-            isSelected={isPlanetInWishlist("Mars")}
-            togglePlanetSelection={() =>
-              togglePlanetSelection("Mars", "/destination/image-mars.png")
-            }
-          />
-          <PlanetCard
-            name="MOON"
-            description="Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers."
-            thumbnail="/destination/image-moon.png"
-            isSelected={isPlanetInWishlist("Moon")}
-            togglePlanetSelection={() =>
-              togglePlanetSelection("Moon", "/destination/image-moon.png")
-            }
-          />
-          <PlanetCard
-            name="TITAN"
-            description="Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets."
-            thumbnail="/destination/image-titan.png"
-            isSelected={isPlanetInWishlist("Titan")}
-            togglePlanetSelection={() =>
-              togglePlanetSelection("Titan", "/destination/image-titan.png")
-            }
-          />
+          {Planets.map((planet) => {
+            return (
+              <PlanetCard
+                key={planet.name}
+                name={planet.name}
+                description={planet.description}
+                thumbnail={planet.thumbnail}
+                isSelected={isPlanetInWishlist(planet.name)}
+                togglePlanetSelection={() =>
+                  togglePlanetSelection(planet.name, planet.thumbnail)
+                }
+              />
+            );
+          })}
         </section>
       </main>
     </div>
